4bcabc3c504e1c8d18eddc2293f4ddc374f87e17
reyes221093 <102719907+reyes221093@users.noreply.github.com>
2022-05-20 14:10:58 +0800

    Add project files.

---
+++ Arraylist_stringbuilder/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+String[] array_name = new string[6];
+array_name[0] = "Jemmie";
+array_name[1] = "Jessica";
+array_name[2] = "Ahl";
+array_name[3] = "alea";
+array_name[4] = "luke";
+array_name[5] = "Arcilla";
+
+foreach (String name in array_name)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine("how many names is listed on the array list?");
+
+string userName = Console.ReadLine();
+
+int time = 6;
+if (time < 4)
+{
+    Console.WriteLine("Correct");
+}
+else
+{
+    Console.WriteLine("the right answer is 6");
+}
